de7dbdbdaf7bbcdc77dbaee3b119e6980bb90c64
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-21 20:29:13 -0300

    :sparkles: feat: Cria service para criar usuario

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const existisUser = await this.prisma.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (existisUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const newUser = new User({
+      email: createUserDto.email,
+      name: createUserDto.name,
+      password: hashPassword,
+      phone: createUserDto.phone,
+    });
+
+    const user = await this.prisma.user.create({
+      data: newUser,
+    });
+
+    return {
+      ...user,
+      password: undefined,
+    };
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    return user;
   }
 }
