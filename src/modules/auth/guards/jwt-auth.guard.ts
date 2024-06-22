f100e754c2641929172b46149699df1ab22a823f
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-22 13:57:39 -0300

    :bug: fix(auth): Remove validacao de decorator dentro do guard de jwt

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -6,8 +6,6 @@ import {
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
-import { $Enums } from '@prisma/client';
-import { IS_ADMIN_KEY } from '../decorators/is-admin.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -16,27 +14,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   }
 
   canActivate(context: ExecutionContext): Promise<boolean> | boolean {
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
-
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    const isAdmin = this.reflector.getAllAndOverride<boolean>(IS_ADMIN_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
     if (isPublic) {
       return true;
-    }
-
-    if (isAdmin) {
-      if (!user || !user.role || !user.role.includes($Enums.Role.ADMIN)) {
-        throw new UnauthorizedException();
-      }
     }
 
     const canActivate = super.canActivate(context);
