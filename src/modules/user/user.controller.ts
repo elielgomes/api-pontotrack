5c1a22614334ece30e50627d4ef3f3a4530ceb0c
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-21 23:25:55 -0300

    :sparkles: feat(auth): Valida credenciais de login com middleware

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { UserFromJwt } from '../auth/models/user-from-jwt';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  getMe(@CurrentUser() user: UserFromJwt) {
+    return user;
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
