8578c5564ceee8656b214dec21e5211158ea2bcd
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-21 21:27:56 -0300

    :sparkles: feat(auth): Implementa login, guard e estrategia local

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
