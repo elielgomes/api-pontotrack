cedaa8178da84fa3354cc9c47c9080f71a7c6e8f
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-23 04:24:15 -0300

    :sparkles: feat(auth): Define raiz da controller de autenticacao como auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -11,7 +11,7 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/auth-request';
 import { IsPublic } from './decorators/is-public.decorator';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
