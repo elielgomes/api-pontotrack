76f8715a060a0a165bdad0916742585f4ecdeff1
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-23 04:23:08 -0300

    :sparkles: feat: Habilita cors na aplicacao

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [process.env.APP_URL, 'http://localhost:3000'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed'));
+      }
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -11,6 +21,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
