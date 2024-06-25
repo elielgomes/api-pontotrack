ed46dcbeadac7b0b157ed7836fc059f0c83c974f
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-06-25 08:22:22 -0300

    :sparkles: feat: Cria dto para validar input de atualizacao de senha de usuario

---
+++ src/modules/user/dto/update-password.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UpdatePasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Invalid password',
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Invalid new password',
+  })
+  newPassword: string;
+}
