1dc3335f7ae71839b92a745763215c32a6bb5afa
Eliel Gomes Hyertquist <elielgomespg@hotmail.com>
2024-07-01 09:30:01 -0300

    :sparkles: feat: Cria rota raiz para mensagem de boas vindas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Head } from '@nestjs/common';
+import { Controller, Get, Head } from '@nestjs/common';
 import { IsPublic } from './modules/auth/decorators/is-public.decorator';
 import { UserService } from './modules/user/user.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly userService: UserService) {}
+
+  @IsPublic()
+  @Get()
+  hello() {
+    return 'Olá, seja bem-vindo à API da AppTrack!';
+  }
+
   @IsPublic()
   @Head('ping')
   ping() {
